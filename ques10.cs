f93256e18920c43081075673df752260ee13652f
Ubaid <mohd.ubaid@successive.tech>
2021-05-07 07:11:11 +0530

    Modified with validation

--- ques10.cs
+++ ques10.cs
@@ -6,31 +6,35 @@ namespace MyApplication
     {
         static void Main(string[] args)
         {
-
-            int[] array = new int[50];
-            int count =0;
-            Console.WriteLine("Enter the number of element you want store in the array");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the element in the array");
-            for (int i = 0; i < n; i++)
+            try
             {
-                array[i] = Convert.ToInt32(Console.Read());
-            }
-
-            for (int i = 0; i < n; i++)
-            {
-                for(int j = i+1; j<n; j++)
+                int[] array = new int[50];
+                int count = 0;
+                Console.WriteLine("Enter the number of element you want store in the array");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the element in the array");
+                for (int i = 0; i < n; i++)
                 {
-                    if(array[i] == array[j])
+                    array[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
                     {
-                        count++;
-                        break;
+                        if (array[i] == array[j])
+                        {
+                            count++;
+                            break;
+                        }
                     }
                 }
+                Console.Write("\nTotal Number of duplicate element : {0} ", count);
             }
-
-            Console.Write("\nTotal Number of duplicate element : {0} " , count );
-            
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- ques5.cs
+++ ques5.cs
@@ -2,26 +2,33 @@
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter the total money you have");
-        int money = Convert.ToInt32(Console.ReadLine());
-        double tax;
-        if (money<10000)
+        try
         {
-            tax = money * 0.05;  //5% tax
-            Console.WriteLine("Total Tax : " + tax );
+            Console.WriteLine("Enter the total money you have");
+            double money = Convert.ToDouble(Console.ReadLine());
+            double tax;
+            if (money < 10000)
+            {
+                tax = money * 0.05;  //5% tax
+                Console.WriteLine("Total Tax : " + tax);
+            }
+            else if (money <= 10000 || money >= 100000)
+            {
+                tax = money * 0.08;  //8% tax
+                Console.WriteLine("Total Tax : " + tax);
+            }
+            else
+            {
+                tax = money * 0.085;  //8.5% tax
+                Console.WriteLine("Total Tax : " + tax);
+            }
         }
-        else if (money<=10000 || money>=100000)
+        catch (Exception e)
         {
-            tax = money * 0.08;  //8% tax
-            Console.WriteLine("Total Tax : " + tax);
+            Console.WriteLine(e.Message);
         }
-        else
-        {
-            tax = money * 0.085;  //8.5% tax
-            Console.WriteLine("Total Tax : " + tax);
-        }
-
     }
 }
