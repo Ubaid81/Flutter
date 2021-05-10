5dbe0df19460a95619ba9afa5fbbfc4cb3e4c260
Ubaid <mohd.ubaid@successive.tech>
2021-05-10 21:28:31 +0530

    Task on File handling

--- filetask.cs
+++ filetask.cs
@@ -12,86 +12,98 @@ namespace FileHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Would You like to Choose the existing file ? if yes , press 1 ");
-            string choice = Console.ReadLine();
-            int count = 0;
-            if(choice == "1")           // for working on the exsisting file in the system
+            try
             {
-                Console.WriteLine("Enter File name (if not know then check in the directory given as name followed by .txt extention): ");
-                string fileName = Console.ReadLine();
-                Console.WriteLine("\n\n");
-                if (File.Exists(fileName))
+                Console.WriteLine("Would You like to Choose the existing file ? if yes , press 1 ");
+                string choice = Console.ReadLine();
+                int count = 0;
+                if (choice == "1")           // for working on the exsisting file in the system
                 {
-                    using (StreamReader sr = File.OpenText(fileName))
+                    Console.WriteLine("Enter File name (if not know then check in the directory given as name followed by .txt extention): ");
+                    string fileName = Console.ReadLine();
+                    Console.WriteLine("\n\n");
+                    if (File.Exists(fileName))
                     {
-                        string s = "";
-                        while ((s = sr.ReadLine()) != null)             //check end of line 
+                        using (StreamReader sr = File.OpenText(fileName))
                         {
-                            count++;                                // increment count we get a line 
+                            string s = "";
+                            while ((s = sr.ReadLine()) != null)             //check end of line 
+                            {
+                                count++;                                // increment count we get a line 
+                            }
                         }
-                    }
-                    Console.WriteLine("Input how many last numbers of lines you want to display ");
-                    int last = Convert.ToInt32(Console.ReadLine());
-                    if (last >= 1 && last <= count)
-                    {
-                        Console.WriteLine("The content of the last {0} lines of the file {1} is : \n", last, fileName);
-                        for (int i = count - last; i < count; i++)
+                        Console.WriteLine("Input how many last numbers of lines you want to display ");
+                        int last = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("\n");
+                       
+                        if (last >= 1 && last <= count)
+                        {
+                            Console.WriteLine("The content of the last {0} lines of the file {1} is : \n", last, fileName);
+                            for (int i = count - last; i < count; i++)
                             {
                                 string[] lines = File.ReadAllLines(fileName);
-                                Console.Write( lines[i] );
+                                Console.Write(lines[i]);
                             }
+                        }
+                        Console.WriteLine("\n");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No such name file found in the directory");
+
+                        Console.WriteLine("\n\n#RUN_AGAIN\n\n");
                     }
                 }
                 else
                 {
-                    Console.WriteLine("No such name file found in the directory");
+                    Console.WriteLine("OKAY! Let's create a new file\n");
 
-                    Console.WriteLine("\n\n#RUN_AGAIN\n\n");
+                    string fileName = "demo.txt";
+                    string[] Lines;
+
+                    if (File.Exists(fileName))
+                    {
+                        File.Delete(fileName);
+                    }
+
+                    Console.WriteLine("Input number of lines to write in the file  : ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    Lines = new string[n];
+                    Console.WriteLine("\nInput {0} strings below :\n", n);
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write("Input line {0} : ", i + 1);
+                        Lines[i] = Console.ReadLine();
+                    }
+                    System.IO.File.WriteAllLines(fileName, Lines);
+
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine("Input last how many numbers of lines you want to display ");
+                    int last = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("\n");
+
+                    if (last >= 1 && last <= n)
+                    {
+                        Console.WriteLine("The content of the last {0} lines of the file {1} is : \n", last, fileName);
+                        for (int i = n - last; i < n; i++)
+                        {
+                            string[] lines = File.ReadAllLines(fileName);
+                            Console.WriteLine(lines[i]);
+                        }
+                    }
+
+                    Console.WriteLine("\n");
+                    Console.WriteLine("\n");
+
                 }
             }
-            else
+            catch(Exception e)
             {
-                Console.WriteLine("OKAY! Let's create a new file\n");
-
-                string fileName =  "demo.txt";
-                string[] Lines;
-
-                if (File.Exists(fileName))
-                {
-                    File.Delete(fileName);
-                }
-
-                Console.WriteLine("Input number of lines to write in the file  : ");
-                int n = Convert.ToInt32(Console.ReadLine());
-                Lines = new string[n];
-                Console.WriteLine("\nInput {0} strings below :\n", n);
-                for (int i = 0; i < n; i++)
-                {
-                    Console.Write("Input line {0} : ", i + 1);
-                    Lines[i] = Console.ReadLine();
-                }
-                System.IO.File.WriteAllLines(fileName, Lines);
-
-                Console.WriteLine("\n");
-
-                Console.WriteLine("Input last how many numbers of lines you want to display ");
-                int last = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("\n");
-
-                if (last >= 1 && last <= n)
-                {
-                    Console.WriteLine("The content of the last {0} lines of the file {1} is : \n", last, fileName);
-                    for (int i = n - last; i < n ; i++)
-                    {
-                        string[] lines = File.ReadAllLines(fileName);
-                        Console.WriteLine(lines[i]);
-                    }
-                }
-
-                Console.WriteLine("\n");
-                Console.WriteLine("\n");
-
+                Console.WriteLine(e.Message);
             }
         }
     }
